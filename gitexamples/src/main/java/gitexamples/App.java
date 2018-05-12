967cb381a7c615cc1cceca9b0216e27d887eaab8
kulasekar Reddy <kulasekar.reddy@cleartrip.com>
2018-05-12 13:22:02 +0530

    new commit

--- gitexamples/src/main/java/gitexamples/App.java
+++ gitexamples/src/main/java/gitexamples/App.java
@@ -3,6 +3,7 @@ package gitexamples;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("testing git");
+	  System.out.println("testing git");
+	  System.out.println("new ");
 	}
 }
