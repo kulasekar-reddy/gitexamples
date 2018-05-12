8e3f701e988b75f518e12618efc9b55569e13af2
kulasekar Reddy <kulasekar.reddy@cleartrip.com>
2018-05-12 13:24:05 +0530

    branch commit

--- gitexamples/src/main/java/gitexamples/App.java
+++ gitexamples/src/main/java/gitexamples/App.java
@@ -5,5 +5,7 @@ public class App {
 	public static void main(String[] args) {
 	  System.out.println("testing git");
 	  System.out.println("new ");
+    System.out.println("new branch changes");
+
 	}
 }
